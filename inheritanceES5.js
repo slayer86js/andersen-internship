0303d6cab5fb7b15e4807f3f57fe8157372c981c
slayer86js <krut1lka86@gmail.com>
2020-11-18 18:45:18 +0300

    Added tasks_inheritance resolve

---
+++ inheritanceES5.js
@@ -0,0 +1,18 @@
+//ES-5 inheritance
+function Person(name) {
+    this.name = name;
+}
+Person.prototype.getName = function() {console.log(this.name)};
+
+let person = new Person('Persona');
+person.getName();
+
+function Man(name) {
+    Person.call(this, name);
+}
+Man.prototype = Object.create(Person.prototype);
+Man.prototype.facialHair = function() {console.log('Man has facial hair')};
+let man = new Man('Man');
+man.getName();
+man.facialHair();
+
---
+++ inheritanceES6.js
@@ -0,0 +1,24 @@
+//ES-6 inheritance
+class Person {
+    constructor(name) {
+        this.name = name;
+    } 
+    getName() {
+        console.log(this.name);
+    }
+}
+
+class Man extends Person {
+    constructor(name) {
+        super(name);
+    }
+    facialHair() {
+        console.log('Man has facial hair')
+    }
+}
+let person = new Person('Persona');
+person.getName();
+let man = new Man('Man');
+man.getName();
+man.facialHair();
+
