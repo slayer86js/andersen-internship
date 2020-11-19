d09f8a6192cc07e41fab2edb41a24b15b840ce1b
slayer86js <krut1lka86@gmail.com>
2020-11-19 16:56:40 +0300

    fixed variable and if

--- server_request.js
+++ server_request.js
@@ -2,9 +2,9 @@
 async function server_request () {
     const response = await fetch('http://www.json-generator.com/api/json/get/cfQCylRjuG')
     const data = await response.json();
-    if (data.getUsersData == true) {
-       const response_2 = await fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC');
-       const fulldata = await response_2.json();
+    if (data.getUsersData) {
+       const responseArray = await fetch('http://www.json-generator.com/api/json/get/cfVGucaXPC');
+       const fulldata = await responseArray.json();
        console.log(fulldata);
     }
 }
