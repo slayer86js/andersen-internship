5bfd9da3e1695d6b8bfb122dee6a8fee8f61335a
slayer86js <krut1lka86@gmail.com>
2020-11-19 18:14:59 +0300

    added task resolve

---
+++ promiseall.js
@@ -0,0 +1,13 @@
+const url = [
+    'http://www.json-generator.com/api/json/get/cevhxOsZnS',
+    'http://www.json-generator.com/api/json/get/cguaPsRxAi',
+    'http://www.json-generator.com/api/json/get/cfDZdmxnDm',
+    'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
+    'http://www.json-generator.com/api/json/get/ceQMMKpidK'
+];
+
+Promise.all(url.map(item => fetch(item)
+  .then(response => response.json())))
+  .then(results => console.log(results))
+
+
