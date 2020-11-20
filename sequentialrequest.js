d5873376960c90dcba7692fa8e44305a33e3c96d
slayer86js <krut1lka86@gmail.com>
2020-11-20 14:46:25 +0300

    fixed reduce method

--- sequentialrequest.js
+++ sequentialrequest.js
@@ -5,22 +5,12 @@ const url = [
     'http://www.json-generator.com/api/json/get/cfkrfOjrfS',
     'http://www.json-generator.com/api/json/get/ceQMMKpidK'
 ];
-// через цикл 
-/*async function server_request(arr) {
-    const data = [];
-    for (let i = 0; i < arr.length; i++) {
-        const item = arr[i];
-        const getData = await fetch(item).then(response => response.json());
-        data.push(getData);
-    }
-    console.log(data);
-}
-server_request(url);*/
 
-//через reduce
-
-Promise.resolve(url.reduce((accum, item) => {return accum
-    .then(res => fetch(item, res)) 
+const data = [];
+url.reduce((accum, item) => accum
+    .then(() => fetch(item)) 
     .then(response => response.json())
-    .then(result => console.log(result))}, Promise.resolve()))
+    .then(result => data.push(result)), Promise.resolve())
+console.log(data);
+
 
